411a044943f120df1dd82f3f6846c98e9fdf4e96
barokool <knabao7a7@gmail.com>
2023-08-12 19:18:18 +0700

    init server

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  async function generatePDF() {
+    try {
+      const response = await fetch("http://localhost:3001/generate-pdf", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email }),
+      });
+
+      if (response.ok) {
+        // Convert the response to a Blob and create a download link
+        const pdfBlob = await response.blob();
+        const url = URL.createObjectURL(pdfBlob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "output.pdf";
+        a.click();
+      } else {
+        console.error("PDF generation failed.");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>PDF Generation Example</h1>
+      <div>
+        <label>Name:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <button onClick={generatePDF}>Generate PDF</button>
     </div>
   );
 }
---
+++ src/types/type.ts
@@ -0,0 +1,10 @@
+export type ExcelResponse = {
+  name: string;
+  email: string;
+  DOB: string;
+  gen: string;
+  address: string;
+  city: string;
+  university: string;
+  company: string;
+};
